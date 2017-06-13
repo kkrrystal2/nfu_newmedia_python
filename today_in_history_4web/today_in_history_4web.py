91a324c92ba6defe46981ec22b7f68acf455cbe1
kkrrystal2 <m15915778956@163.com>
2017-06-13 22:53:26 +0800

    our

--- today_in_history_4web/today_in_history_4web.py
+++ today_in_history_4web/today_in_history_4web.py
@@ -4,29 +4,29 @@ from lookup_longitude_latitude import get_img
 
 app = Flask(__name__)
 
-with open ('data/guangdong.txt','r',encoding='utf8')as data:
-    gd=data.readlines()
+with open ('data/date.txt','r',encoding='utf8')as data:
+    date=data.readlines()
 data_all=[]
-for item in gd:
+for item in date:
     item_data = item.strip().split(' ')
     item_dict = {x.split(':')[0]:x.split(':')[1] for x in item_data}
     data_all.append(item_dict)
 
-city_list=[x['城市'] for x in data_all]
+date_list=[x['城市'] for x in data_all]
 
-@app.route('/pick_city', methods=['POST'])
+@app.route('/pick_date', methods=['POST'])
 
 def do_search() -> 'html':
 
     """Extract the posted data; perform the search; return results."""
 
-    city=request.form['city']
+    date=request.form['date']
 
     title = '以下是您的结果：'
-    results = get_img(city)
+    results = get_img(date)
     return render_template('results.html',
                            the_title=title,
-                           the_city=city,
+                           the_city=date,
                            the_results=results)
 
 
@@ -35,8 +35,8 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
     return render_template('entry.html',
-                           the_title='广东省城市地图！',
-						   the_user_city=city_list)
+                           the_title='历史上的今天查询！',
+						   the_user_date=date_list)
 
 						   
 #这里我们接受一个 filename 檔名变量，http://127.0.0.1:5000/img/檔名
