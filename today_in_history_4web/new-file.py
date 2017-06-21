08047b1ee796b80921de15ae019405e7ed3659e1
gigiily000 <630316031@qq.com>
2017-06-21 18:06:18 +0800

    Update new-file.py

--- today_in_history_4web/new-file.py
+++ today_in_history_4web/new-file.py
@@ -1,4 +1,4 @@
-import urllib.request, json
+buimport urllib.request, json
 
 def get_history(date):
     print ('''
@@ -18,4 +18,4 @@ def get_history(date):
         return [_id,title,pic,year,month,day,des,lunar,]
 
     except NameError :
-        print('存在此日期')
+        print('不存在此日期')
