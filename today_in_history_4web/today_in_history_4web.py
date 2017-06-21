d04ec4a7cac0bb989ecfbc5e2be33cc380a9fc25
kkrrystal2 <m15915778956@163.com>
2017-06-21 14:59:52 +0800

    3ch

--- today_in_history_4web/today_in_history_4web.py
+++ today_in_history_4web/today_in_history_4web.py
@@ -8,18 +8,6 @@ app = Flask(__name__)
 @app.route('/pick_a_date', methods=['POST'])
 def show_content() -> 'html':
     """Extract the posted data; perform the search; return results."""
-    
-import requests
-r = requests.get('http://api.juheapi.com/japi/toh?key=4bc027ace0535ecf7e935870a1b9deef&v=1.0&month=11&day=1')
-gg=r.text
-aa= eval(gg)
-aa['result'][0]['des']
-len(aa['result'])
-kkkf=[]
-for i in range(len(aa['result'])):
-kki=aa['result'][i]['des']
-kkkf.append(kki)
-
     date = request.form['date']
     results = today_in_history
     return render_template('results.html',
