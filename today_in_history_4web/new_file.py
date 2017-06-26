9fc7fca57e0235afadb26b3c3ed82b85caf66346
kkrrystal2 <m15915778956@163.com>
2017-06-26 12:06:16 +0800

    somgthin

---
+++ today_in_history_4web/new_file.py
@@ -0,0 +1,13 @@
+import requests, json
+
+def get_data(month,day):
+        contents=[]
+        url=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=11&day=1".format(month,day))
+        import requests
+        r = requests.get(url)
+        data=r.json()
+        results=data['result']
+        #print(results[:5]) +each_i['title']+each_i['des']
+        for each_i in results[:5]:#打印其标题及其梗概
+           contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
+        return contents
--- today_in_history_4web/today_in_history_4web.py
+++ today_in_history_4web/today_in_history_4web.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*- 
 from flask import Flask, render_template, request, escape
-
+from new_file import get_data
 app = Flask(__name__)
 
 
 
-@app.route('/pick_a_date', methods=['POST'])
+@app.route('/content', methods=['POST'])
 def show_content() -> 'html':
-    """Extract the posted data; perform the search; return results."""
-    date = request.form['date']
-    results = today_in_history
+    month_day = list(request.form['month_day'])
+    month = month_day[0]
+    day = month_day[1]
+    results = get_data(month,day)
     return render_template('results.html',
                            the_results=results,
-                           the_date = date,
-                           the_history = results[1],
+                           #the_history = results[1],
 
                            )
 
