a3c6b34dac510bad7584d06be4339b91f040b8be
kkrrystal2 <m15915778956@163.com>
2017-06-30 16:46:21 +0800

    more#

--- today_in_history_4web/new_file.py
+++ today_in_history_4web/new_file.py
@@ -2,14 +2,27 @@ import requests, json
 
 def get_data(month,day):
         contents=[]
-        url1=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=7&day=4".format(month,day))
-        url2=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=10&day=1".format(month,day))
+        url=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=7&day=4".format(month,day))
         import requests
-        r = requests.get(url1)
-        r = requests.get(url2)
+        r = requests.get(url)
         data=r.json()
         results=data['result']
         #print(results[:5]) +each_i['title']+each_i['des']
         for each_i in results[:5]:#打印其标题及其梗概
            contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
         return contents
+
+import requests, json
+
+def get_data(month,day):
+        contents=[]
+        url=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=10&day=1".format(month,day))
+        import requests
+        r = requests.get(url)
+        data=r.json()
+        results=data['result']
+        #print(results[:5]) +each_i['title']+each_i['des']
+        for each_i in results[:5]:#打印其标题及其梗概
+           contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
+        return contents
+
