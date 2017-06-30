def969476b2cfab931593d0ace4bac7da9d24b63
kkrrystal2 <m15915778956@163.com>
2017-06-30 16:37:02 +0800

    useful

--- today_in_history_4web/new_file.py
+++ today_in_history_4web/new_file.py
@@ -12,7 +12,7 @@ def get_data(month,day):
            contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
         return contents
 
-        def get_data(month,day):
+def get_data(month,day):
         contents=[]
         url=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=10&day=1".format(month,day))
         import requests
