7ddde7db116e450d8cab8c197ad38b1326c1e7c4
kkrrystal2 <m15915778956@163.com>
2017-06-30 16:33:14 +0800

    6aa

--- today_in_history_4web/new_file.py
+++ today_in_history_4web/new_file.py
@@ -11,3 +11,15 @@ def get_data(month,day):
         for each_i in results[:5]:#打印其标题及其梗概
            contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
         return contents
+		
+        def get_data(month,day):
+        contents=[]
+        url=("http://api.juheapi.com/japi/toh?key=1a21b2aace47c4fc71c446f7b025fe47&v=1.0&month=10&day=1".format(month,day))
+        import requests
+        r = requests.get(url)
+        data=r.json()
+        results=data['result']
+        #print(results[:5]) +each_i['title']+each_i['des']
+        for each_i in results[:5]:#打印其标题及其梗概
+           contents.append(str(each_i['month'])+'月'+str(each_i['day'])+'日'+each_i['title']+each_i['des'])
+        return contents
